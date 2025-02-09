5b72a3b27507a50cb6f0ae11c9b43f633a8a5d64
Ucet-Sandhiya <s.wptsandhi2002@gmail.com>
2025-02-09 20:26:02 +0530

    my commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from "./pages/Home"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-       i am sandhiya
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>sandhiya</h1>
+
     </div>
   );
 }
-
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+function Home(){  
+    return {
+   <h2>sandhioaydsigf</h2>
+    };
+    
+
+}
+export default Home;
